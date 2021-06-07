806a4bf00707a110edf1fbbb6ae07e63ace6caec
zhen.wang <zhen.wang@fa-software.com>
2021-06-07 15:54:44 +0800

    第二次提交

--- coupon/src/main/java/com/wz/coupon/controller/CouponController.java
+++ coupon/src/main/java/com/wz/coupon/controller/CouponController.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,6 +25,7 @@ import com.wz.common.utils.R;
  * @email wangzhen404@163.com
  * @date 2021-05-27 21:55:50
  */
+@RefreshScope
 @RestController
 @RequestMapping("coupon/coupon")
 public class CouponController {
---
+++ gateway/src/main/java/com/wz/gateway/GatewayApplication.java
@@ -0,0 +1,19 @@
+package com.wz.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * 1.开启服务注册发现
+ */
+@EnableDiscoveryClient
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+public class GatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+
+}
